147a03c95c69e49680a1bcff98f022daec615cbe
Victor Okumoto <okumotovictor@gmail.com>
2024-02-23 15:02:45 -0300

    finalização do projeto

--- js/app.js
+++ js/app.js
@@ -2,11 +2,13 @@ function alterarStatus(id) {
     let jogoSelecionado = document.getElementById(`game-${id}`);
     let imagem = jogoSelecionado.querySelector('.dashboard__item__img');
     let botao = jogoSelecionado.querySelector('.dashboard__item__button');
+    let nomeJogo = jogoSelecionado.querySelector('.dashboard__item__name');
 
     if (imagem.classList.contains('dashboard__item__img--rented')) {
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent = 'Alugar';
+        alert(`${nomeJogo.textContent} foi devolvido com sucesso!`);
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
